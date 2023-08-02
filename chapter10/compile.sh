c76f9f5e6bb5beefc8a9aa8602ba086496ebdc6f
Marco Netto <marco.netto@gmail.com>
2023-08-01 17:20:46 -0700

    export some variables to the setup of mpirun

--- chapter10/compile.sh
+++ chapter10/compile.sh
@@ -7,10 +7,14 @@ echo "Creating run_mpi.sh file"
 
 MPI_EXE=mpi_show_hosts
 MPI_CODE=mpi_show_hosts.c
-
 MPI_EXE_PATH="${AZ_BATCH_NODE_MOUNTS_DIR}/data/"
-cat << 'EOF' > run_mpi.sh
+
+cat << EOF > run_mpi.sh
 #!/bin/bash
+
+MPI_EXE=${MPI_EXE}
+MPI_CODE=${MPI_CODE}
+MPI_EXE_PATH=${MPI_EXE_PATH}
 
 if [ -f /etc/bashrc ]; then
     . /etc/bashrc
@@ -22,22 +26,24 @@ module load mpi/hpcx
 # Create host file
 batch_hosts=hosts.batch
 rm -rf $batch_hosts
-IFS=';' read -ra ADDR <<< "$AZ_BATCH_NODE_LIST"
-for i in "${ADDR[@]}"; do echo $i >> $batch_hosts;done
+IFS=';' read -ra ADDR <<< "\$AZ_BATCH_NODE_LIST"
+
+for i in "\${ADDR[@]}"; do echo \$i >> \$batch_hosts;done
 
 # Determine hosts to run on
-src=$(tail -n1 $batch_hosts)
-dst=$(head -n1 $batch_hosts)
-echo "Src: $src"
-echo "Dst: $dst"
+src=\$(tail -n1 \$batch_hosts)
+dst=\$(head -n1 \$batch_hosts)
+echo "Src: \$src"
+echo "Dst: \$dst"
 
 NODES=2
 PPN=2
-NP=$(($NODES*$PPN))
+NP=\$((\$NODES*\$PPN))
 
 set -x
 
-mpirun -np $NP --oversubscribe --host ${src}:${PPN},${dst}:${PPN} --map-by ppr:${PPN}:node --mca btl tcp,vader,self --mca coll_hcoll_enable 0 --mca btl_tcp_if_include lo,eth0 --mca pml ^ucx ${MPI_EXE_PATH}/${MPI_EXE}
+mpirun -np \$NP --oversubscribe --host \${src}:\${PPN},\${dst}:\${PPN} --map-by ppr:\${PPN}:node --mca btl tcp,vader,self --mca coll_hcoll_enable 0 --mca btl_tcp_if_include lo,eth0 --mca pml ^ucx ${MPI_EXE_PATH}/${MPI_EXE}
+
 EOF
 
 chmod +x run_mpi.sh

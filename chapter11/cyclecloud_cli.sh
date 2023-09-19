9549f444a906a165e23fda3faa9a95ddd153437f
Marco Netto <marco.netto@gmail.com>
2023-09-19 14:24:26 -0700

    use keyvault to store and retrieve password and ssh key

--- chapter11/cyclecloud_cli.sh
+++ chapter11/cyclecloud_cli.sh
@@ -4,6 +4,7 @@
 RG=mydemo
 SKU=Standard_B2ms
 VMIMAGE=microsoft-dsvm:ubuntu-hpc:1804:18.04.2021120101
+CCIMAGE=azurecyclecloud:azure-cyclecloud:cyclecloud8:latest
 REGION=eastus
 
 STORAGEACCOUNT="$RG"sa
@@ -17,6 +18,8 @@ VMNAME="$RG"vm
 VMVNETNAME="$RG"VNET
 VMSUBNETNAME="$RG"SUBNET
 ADMINUSER=azureuser
+
+KEYVAULT="$RG"kv
 
 CLOUDINITFILE=cloudinit.file
 
@@ -36,16 +39,20 @@ function create_vnet_subnet(){
 }
 
 
+function create_keyvault(){
+
+    az keyvault create --resource-group $RG \
+                   --name $KEYVAULT \
+                   --location "$REGION" \
+                   --enabled-for-deployment true \
+                   --enabled-for-disk-encryption true \
+                   --enabled-for-template-deployment true
+}
+
+
 function create_cloud_init(){
 
-    if [ -z ${CCPASSWORD+x} ] ; then
-        echo "CCPASSWORD: must be set and non-empty"
-        exit
-    fi
-    if [ -z ${CCPUBKEY+x} ] ; then
-        echo "CCPUBKEY: must be set and non-empty"
-        exit
-    fi
+
 
     account_info=$(az account show)
 
@@ -57,45 +64,43 @@ function create_cloud_init(){
     cyclecloud_storage_container=cyclecloud
     cyclecloud_location=$REGION
     cyclecloud_rg=$RG
-    cyclecloud_admin_password=$CCPASSWORD
-    cyclecloud_admin_public_key=$CCPUBKEY
 
 cat << EOF > $CLOUDINITFILE
 
 #cloud-config
 
 runcmd:
-  #
-  # Install CycleCloud
-  #
-  - apt -y install wget gnupg2
-  - wget -qO - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-  - echo 'deb https://packages.microsoft.com/repos/cyclecloud bionic main' > /etc/apt/sources.list.d/cyclecloud.list
-  - apt update
-  - apt-get install -yq openjdk-8-jdk
-  - update-java-alternatives -s java-1.8.0-openjdk-amd64
-  - apt-get install -yq python3-venv
-  - apt-get install -yq python3.8-venv
-  - apt-get install -yq cyclecloud8=8.3.0-3062
-  # Because /opt/cycle_server/config/ already exists, you need to force install
-  - bash \`find /opt/cycle_server/.installer -maxdepth 1 -type d | grep '/opt/cycle_server/.installer/'\`/install.sh --force
-  - /opt/cycle_server/cycle_server await_startup
-  # Install CycleCloud CLI
-  - unzip /opt/cycle_server/tools/cyclecloud-cli.zip -d /tmp
-  - python3 /tmp/cyclecloud-cli-installer/install.py -y --installdir /home/${cyclecloud_admin_name}/.cycle --system
-  # Separate command to avoid potential quote / double quote / var expansion troubles
-  - cmd="/usr/local/bin/cyclecloud initialize --loglevel=debug --batch --url=http://localhost:8080 --verify-ssl=false --username=${cyclecloud_admin_name} --password='${cyclecloud_admin_password}'"
-  # Must run as user or CycleCloud will attempt to install in /root/.cycle
-  - runuser -l ${cyclecloud_admin_name} -c "\$cmd"
-  - runuser -l ${cyclecloud_admin_name} -c '/usr/local/bin/cyclecloud account create -f /opt/cycle_server/azure_subscription.json'
-  - rm -f /opt/cycle_server/config/data/cyclecloud_account.json.imported
+    - apt-get -y install gnupg2
+    - wget -qO - https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+    - echo 'deb https://packages.microsoft.com/repos/cyclecloud bionic main' > /etc/apt/sources.list.d/cyclecloud.list
+    - apt-get update
+    - apt-get install -yq openjdk-8-jdk
+    - update-java-alternatives -s java-1.8.0-openjdk-amd64
+    - apt-get install -yq python3-venv
+    - echo "Install Azure CLI"
+    - curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+    - which az
+    - az login --identity --allow-no-subscriptions
+    - CCPASSWORD=\$(az keyvault secret show --name ccpassword --vault-name $KEYVAULT --query 'value' -o tsv)
+    - CCPUBKEY=\$(az keyvault secret show --name ccpubkey --vault-name $KEYVAULT --query 'value' -o tsv)
+    - apt-get install -yq cyclecloud8=8.3.0-3062
+    - escaped_CCPASSWORD=\$(printf '%s\n' "\$CCPASSWORD" | sed -e 's/[]\/\$*.^[]/\\\&/g')
+    - escaped_CCPUBKEY=\$(printf '%s\n' "\$CCPUBKEY" | sed -e 's/[]\/\$*.^[]/\\\&/g')
+    - sed -i "s/CCPASSWORD/\$escaped_CCPASSWORD/g" /tmp/cyclecloud_account.json
+    - sed -i "s/CCPUBKEY/\$escaped_CCPUBKEY/g" /tmp/cyclecloud_account.json
+    - mv /tmp/cyclecloud_account.json /opt/cycle_server/config/data/cyclecloud_account.json
+    - /opt/cycle_server/cycle_server await_startup
+    - unzip /opt/cycle_server/tools/cyclecloud-cli.zip -d /tmp
+    - python3 /tmp/cyclecloud-cli-installer/install.py -y --installdir /home/${cyclecloud_admin_name}/.cycle --system
+    - cmd="/usr/local/bin/cyclecloud initialize --loglevel=debug --batch --url=http://localhost:8080 --verify-ssl=false --username=${cyclecloud_admin_name} --password='\$CCPASSWORD'"
+    - runuser -l ${cyclecloud_admin_name} -c "\$cmd"
+    - mv /tmp/azure_subscription.json /opt/cycle_server/azure_subscription.json
+    - runuser -l ${cyclecloud_admin_name} -c '/usr/local/bin/cyclecloud account create -f /opt/cycle_server/azure_subscription.json'
+    - rm -f /opt/cycle_server/config/data/cyclecloud_account.json.imported
 
 write_files:
-  - path: /opt/cycle_server/config/java_home
-    content: |
-      /usr/local/openjdk-8
 
-  - path: /opt/cycle_server/config/data/cyclecloud_account.json
+  - path: /tmp/cyclecloud_account.json
     content: |
       [
         {
@@ -106,14 +111,14 @@ write_files:
         {
           "AdType": "AuthenticatedUser",
           "Name": "${cyclecloud_admin_name}",
-          "RawPassword": "${cyclecloud_admin_password}",
+          "RawPassword": "CCPASSWORD",
           "Superuser": true
         },
         {
           "AdType": "Credential",
           "CredentialType": "PublicKey",
           "Name": "${cyclecloud_admin_name}/public",
-          "PublicKey": "${cyclecloud_admin_public_key}"
+          "PublicKey": "CCPUBKEY"
         },
         {
           "AdType": "Application.Setting",
@@ -122,7 +127,7 @@ write_files:
         }
       ]
 
-  - path: /opt/cycle_server/azure_subscription.json
+  - path: /tmp/azure_subscription.json
     content: |
       {
         "Environment": "public",
@@ -149,7 +154,6 @@ EOF
 
 function create_vm() {
 
-
     az vm create -n $VMNAME \
         -g $RG \
         --image $VMIMAGE \
@@ -161,7 +165,6 @@ function create_vm() {
         --assign-identity \
         --generate-ssh-keys \
         --custom-data $CLOUDINITFILE
-
 }
 
 function peer_vpn(){
@@ -192,7 +195,7 @@ function create_storage_account(){
           --sku Standard_LRS
 }
 
-function add_vm_permission(){
+function add_vm_permission_subscription(){
 
     account_info=$(az account show)
     subscription=$(echo $account_info | jq -r '.id')
@@ -212,11 +215,90 @@ function add_vm_permission(){
 
 }
 
+function add_vm_permission_keyvault(){
+
+    VMPrincipalID=$(az vm show \
+                         -g $RG \
+                         -n "$VMNAME" \
+                         --query "identity.principalId" \
+                         -o tsv)
+
+    echo $VMPrincipalID
+
+    az keyvault set-policy --resource-group $RG \
+                       --name $KEYVAULT \
+                       --object-id $VMPrincipalID \
+                       --key-permissions all \
+                       --secret-permissions all
+}
+
+function get_password_manually(){
+
+    unset CCPASSWORD
+    echo -n "Enter password: "
+    while IFS= read -p "$prompt" -r -s -n 1 char
+    do
+        if [[ $char == $'\0' ]] ; then
+            break
+        fi
+        if [[ $char == $'\177' ]] ; then
+            prompt=$'\b \b'
+            CCPASSWORD="${CCPASSWORD%?}"
+        else
+            prompt='*'
+            CCPASSWORD+="$char"
+        fi
+    done
+    echo -e "\npassword=$CCPASSWORD"
+}
+
+
+function validate_secret_availability(){
+
+    if [ -z ${CCPASSWORD+x} ] ; then
+        echo "CCPASSWORD: must be set and non-empty"
+        echo "Control-C to cancel and set CCPASSWORD in environment variable"
+        get_password_manually
+    else
+        echo "Got CCPASSWORD from environment variable"
+    fi
+
+    if [ -z ${CCPUBKEY+x} ] ; then
+        echo "CCPUBKEY: must be set and non-empty"
+        PUBKEYFILE="$HOME/.ssh/id_rsa.pub"
+        if [ -f "$PUBKEYFILE" ]; then
+            read -p "[$PUBKEYFILE] Can I get the pub key from here [Y|n]? " yn
+            if [ -z "$yn" ] || [ "$yn" == "Y" ] || [ "$yn" == "y" ]; then
+                echo "Happy for you! Will use this key!"
+                CCPUBKEY=$(cat $PUBKEYFILE)
+            else
+                echo "Without CCPUBKEY I cannot continue..."
+                exit
+            fi
+        fi
+    else
+        echo "Got CCPUBKEY from environment variable"
+    fi
+
+}
+
+
+function set_keyvault_secrets(){
+
+    az keyvault secret set --name ccpassword --vault-name $KEYVAULT --value "$CCPASSWORD" > /dev/null
+    az keyvault secret set --name ccpubkey --vault-name $KEYVAULT --value "$CCPUBKEY" > /dev/null
+}
+
+validate_secret_availability
+
 create_resource_group
 create_vnet_subnet
 peer_vpn
 create_storage_account
+create_keyvault
 
 create_cloud_init
+set_keyvault_secrets
 create_vm
-add_vm_permission
+add_vm_permission_subscription
+add_vm_permission_keyvault

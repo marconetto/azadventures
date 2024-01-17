fbfc3cc60a1bdf90c04c07177e02a66b763d5f29
Marco Netto <marco.netto@gmail.com>
2024-01-17 13:55:57 -0800

    fix syntax

--- chapter3/create_peering_vpn.sh
+++ chapter3/create_peering_vpn.sh
@@ -31,12 +31,12 @@ echo "VMVNET=$VMVNET"
 echo "RGGWVNET=$RGGWVNET"
 echo "GWVNET=$GWVNET"
 
-echo "GWVNET_TO_VMVNET"="$GWVNET_TO_VMVNET"
-echo "VMVNET_TO_GWVNET"="$VMVNET_TO_GWVNET"
+echo "GWVNET_TO_VMVNET"="${GWVNET_TO_VMVNET}"
+echo "VMVNET_TO_GWVNET"="${VMVNET_TO_GWVNET}"
 
 # GW VNET TO VM VNET
 vmvnetid=$(az network vnet show \
-  --resource-group "$RGVMVNET" \
+  --resource-group "${RGVMVNET}" \
   --name "$VMVNET" \
   --query id --out tsv)
 
@@ -44,10 +44,10 @@ echo "VM VNET ID=$vmvnetid"
 
 set -x
 az network vnet peering create \
-  --name "$GWVNET_TO_VMVNET" \
-  --resource-group "$RGGWVNET" \
-  --vnet-name "$GWVNET" \
-  --remote-vnet "'$vmvnetid'" \
+  --name "${GWVNET_TO_VMVNET}" \
+  --resource-group "${RGGWVNET}" \
+  --vnet-name "${GWVNET}" \
+  --remote-vnet "'${vmvnetid}'" \
   --allow-vnet-access \
   --allow-gateway-transit \
   --allow-forwarded-traffic
@@ -57,18 +57,18 @@ check_error $?
 
 # VM VNET TO GW VNET
 gwvnetid=$(az network vnet show \
-  --resource-group "$RGGWVNET" \
-  --name "$GWVNET" \
+  --resource-group "${RGGWVNET}" \
+  --name "${GWVNET}" \
   --query id --out tsv)
 
 echo "GW VNET ID=$gwvnetid"
 
 set -x
 az network vnet peering create \
-  --name "$VMVNET_TO_GWVNET" \
-  --resource-group "$RGVMVNET" \
-  --vnet-name "$VMVNET" \
-  --remote-vnet "'$gwvnetid'" \
+  --name "${VMVNET_TO_GWVNET}" \
+  --resource-group "${RGVMVNET}" \
+  --vnet-name "${VMVNET}" \
+  --remote-vnet "'${gwvnetid}'" \
   --allow-vnet-access \
   --allow-forwarded-traffic \
   --use-remote-gateways
@@ -76,5 +76,5 @@ set +x
 
 check_error $?
 
-az network vnet peering list -g "$RGGWVNET" --vnet-name "$GWVNET" -o table
-az network vnet peering list -g "$RGVMVNET" --vnet-name "$VMVNET" -o table
+az network vnet peering list -g "${RGGWVNET}" --vnet-name "${GWVNET}" -o table
+az network vnet peering list -g "${RGVMVNET}" --vnet-name "${VMVNET}" -o table

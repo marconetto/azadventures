02ed3d85459bf18fea49c42497fd638a51d2ad2e
Marco Netto <marco.netto@gmail.com>
2024-02-16 14:36:19 -0800

    add wrf run scripts

--- chapter12/cc_wrfconus/specs/default/cluster-init/scripts/00_get_conus.sh
+++ chapter12/cc_wrfconus/specs/default/cluster-init/scripts/00_get_conus.sh
@@ -7,3 +7,5 @@ runuser -l "$ADMINUSER" -c 'tar jxvf bench_12km.tar.bz2'
 
 runuser -l "$ADMINUSER" -c 'curl -O https://www2.mmm.ucar.edu/wrf/users/benchmark/v3911/bench_2.5km.tar.bz2'
 runuser -l "$ADMINUSER" -c 'tar jxvf bench_2.5km.tar.bz2'
+runuser -l "$ADMINUSER" -c 'curl -O https://raw.githubusercontent.com/marconetto/azadventures/main/chapter12/run_wrf_eth_12km.sh'
+runuser -l "$ADMINUSER" -c 'curl -O https://raw.githubusercontent.com/marconetto/azadventures/main/chapter12/run_wrf_hb_2_5km.sh'

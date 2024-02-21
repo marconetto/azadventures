5189d20beceb1256c52bcef4eaeefdd7f6215607
Marco Netto <marco.netto@gmail.com>
2024-02-21 11:58:19 -0800

    add flag to show commands

--- chapter12/cc_wrfconus/specs/default/cluster-init/scripts/00_get_conus.sh
+++ chapter12/cc_wrfconus/specs/default/cluster-init/scripts/00_get_conus.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+set -x
+
 ADMINUSER=$(grep name /opt/cycle/jetpack/config/auth.json | awk -F'"' '{print $4}')
+
+runuser -l "$ADMINUSER" -c 'curl -O https://raw.githubusercontent.com/marconetto/azadventures/main/chapter12/run_wrf_eth_12km.sh'
+runuser -l "$ADMINUSER" -c 'curl -O https://raw.githubusercontent.com/marconetto/azadventures/main/chapter12/run_wrf_hb_2_5km.sh'
 
 runuser -l "$ADMINUSER" -c 'curl -O https://www2.mmm.ucar.edu/wrf/users/benchmark/v3911/bench_12km.tar.bz2'
 runuser -l "$ADMINUSER" -c 'tar jxvf bench_12km.tar.bz2'
 
 runuser -l "$ADMINUSER" -c 'curl -O https://www2.mmm.ucar.edu/wrf/users/benchmark/v3911/bench_2.5km.tar.bz2'
 runuser -l "$ADMINUSER" -c 'tar jxvf bench_2.5km.tar.bz2'
-runuser -l "$ADMINUSER" -c 'curl -O https://raw.githubusercontent.com/marconetto/azadventures/main/chapter12/run_wrf_eth_12km.sh'
-runuser -l "$ADMINUSER" -c 'curl -O https://raw.githubusercontent.com/marconetto/azadventures/main/chapter12/run_wrf_hb_2_5km.sh'

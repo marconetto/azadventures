aeed695f30adae84349af7413ce55a748396f2c7
Marco Netto <marco.netto@gmail.com>
2024-04-03 11:36:03 -0700

    add script to download conus rather than downloading it

--- chapter12/cc_wrfconus/specs/default/cluster-init/scripts/00_get_conus.sh
+++ chapter12/cc_wrfconus/specs/default/cluster-init/scripts/00_get_conus.sh
@@ -10,5 +10,6 @@ runuser -l "$ADMINUSER" -c 'curl -O https://raw.githubusercontent.com/marconetto
 runuser -l "$ADMINUSER" -c 'curl -O https://www2.mmm.ucar.edu/wrf/users/benchmark/v44/v4.4_bench_conus12km.tar.gz'
 runuser -l "$ADMINUSER" -c 'tar zxvf v4.4_bench_conus12km.tar.gz ; rm v4.4_bench_conus12km.tar.gz'
 
-runuser -l "$ADMINUSER" -c 'curl -O https://www2.mmm.ucar.edu/wrf/users/benchmark/v44/v4.4_bench_conus2.5km.tar.gz'
-runuser -l "$ADMINUSER" -c 'tar zxvf v4.4_bench_conus2.5km.tar.gz ; rm v4.4_bench_conus2.5km.tar.gz'
+runuser -l "$ADMINUSER" -c 'echo "curl -O https://www2.mmm.ucar.edu/wrf/users/benchmark/v44/v4.4_bench_conus2.5km.tar.gz" >> download_wrf_hb_2_5km.sh'
+#runuser -l "$ADMINUSER" -c 'curl -O https://www2.mmm.ucar.edu/wrf/users/benchmark/v44/v4.4_bench_conus2.5km.tar.gz'
+#runuser -l "$ADMINUSER" -c 'tar zxvf v4.4_bench_conus2.5km.tar.gz ; rm v4.4_bench_conus2.5km.tar.gz'
